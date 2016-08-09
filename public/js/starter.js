97814584774c6aa51328fa247680ce8f92466b64
Joe Fryd <joescf@gmail.com>
2016-08-09 10:53:22 -0400

    got multiple markers to show up

--- public/js/starter.js
+++ public/js/starter.js
@@ -1,6 +1,6 @@
     // from google maps api
     // create the map and place it on the page
-    var map;
+    let map;
       function initMap() {
         navigator.geolocation.getCurrentPosition(function(location) {
         let latitude = location.coords.latitude;
@@ -21,7 +21,6 @@
       });
     };
 
-
 $('document').ready(function() {
 
 console.log('conneected');
@@ -31,22 +30,59 @@ $('#button').click(function() {
   let latitudeFlag = location.coords.latitude;
   let longitudeFlag = location.coords.longitude;
     console.log(location);
-
-    console.log('latitude: ' + latitudeFlag);
-    console.log('longitude: ' + longitudeFlag);
-    var marker = new google.maps.Marker({
-        position: {lat: latitudeFlag, lng: longitudeFlag},
-        map: map,
-        title: 'You are here'
-      });
+    console.log(google);
 
     let latInput = $('<input class="coords" name="latitude">').val(latitudeFlag);
     let longInput = $('<input class="coords" name="longitude">').val(longitudeFlag);
     $('form').append(latInput, longInput);
 
+// The following functions was taken from the following website
+// http://stackoverflow.com/questions/7701077/add-marker-function-with-google-maps-api
+
+  function addMarker(location) {
+    console.log(theDrop);
+      marker = new google.maps.Marker({
+          position: location,
+          map: map,
+          title: theDrop
+      });
+    }
+
+   function TestMarker() {
+    let theLats;
+    let theLngs;
+    let drop;
+      $.ajax({
+        url: '/drops',
+        type: 'GET',
+        dataType: 'json',
+        data: {latitude: '',
+               longitude: ''},
+      })
+      .done(function(data) {
+        console.log("success");
+        console.log(data);
+        for (let i = 0; i < data.length; i++) {
+          theLats = data[i].latitude;
+          theLngs = data[i].longitude;
+          theDrop = data[i].drop;
+          allMarkers = new google.maps.LatLng(theLats, theLngs, theDrop);
+          addMarker(allMarkers);
+        }
+
+      })
+      .fail(function() {
+        console.log("error");
+      })
+      .always(function() {
+        console.log("complete");
+      });
+    }
+    TestMarker();
+
+
   });
 
 });
-
 
 });
